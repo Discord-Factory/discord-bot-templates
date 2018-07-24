be3b6b20b2f4f6ef416134b50101ecdf0f97f3fc
PoeticShoko <32654584+PoeticShoko@users.noreply.github.com>
2018-07-23 20:48:30 -0600

    🏓

--- exports/commands/ping.js
+++ exports/commands/ping.js
@@ -1,1 +1,16 @@
+//listado de dependecias que usara el comando
+const Discord = require('discord.js')
 
+exports.run = (bot, message, args) => {
+  
+    let ping = Math.floor(message.client.ping);
+  
+message.channel.send(":ping_pong: Pong!")
+.then(m => { 
+     const embed = new Discord.RichEmbed()
+      .addField(`:incoming_envelope: Ping Mensajes: `, `${m.createdTimestamp - message.createdTimestamp} ms`, true)
+      .addField(`:satellite_orbital: Ping DiscordAPI:` , `${ping} ms`, true)
+      .setColor(0x8600b3)                
+     m.edit({embed}); 
+  })
+}  
